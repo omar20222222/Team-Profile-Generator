a2d3ecefc57ce0160312ad6ac4a9477a2afe6f24
omar20222222 <108297683+omar20222222@users.noreply.github.com>
2022-12-01 20:35:59 +0000

    index.html update

---
+++ Lib/Supervisor.js
@@ -0,0 +1,26 @@
+describe("Getters", () => {
+  const name = " Abdurahman Ahmed";
+  const id = 220;
+  const email = "Abdurahman.ahmed@hotmail.com";
+  const role = "Supervisor";
+
+  const testEmployee = new Employee(name, id, email);
+
+  it("Should return the Employee name when requested", () => {
+    expect(testEmployee.getName()).toBe(name);
+  });
+
+  it("Should return the Employee id when requested", () => {
+    expect(testEmployee.getId()).toBe(id);
+  });
+
+  it("Should return the Employee email when requested", () => {
+    expect(testEmployee.getEmail()).toBe(email);
+  });
+
+  it("Should return the Employee role when requested", () => {
+    expect(testEmployee.getRole()).toBe(role);
+
+    const Employee = require("./Supervisor.js");
+  });
+});
